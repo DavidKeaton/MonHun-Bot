cf549e01acffd25db895c609077181d89aa362eb
DavidKeaton <DavekDOS@gmail.com>
2017-02-22 17:39:17 -0500

    template arrangement for core commands

---
+++ src/Commands/Core.java
@@ -0,0 +1,39 @@
+package info.davek.mhbot.Commands;
+
+/**
+ * Core commands used by our Discord bot.
+ */
+public class Core
+{
+	private static final Command[] commands = {
+		new Command("help",
+		            "Display ",
+		            null),
+
+		new Command("",
+		            "",
+		            null),
+
+		new Command("",
+		            "",
+		            null),
+
+		new Command("",
+		            "",
+		            null),
+
+		new Command("",
+		            "",
+		            null),
+
+		new Command("",
+		            "",
+		            null),
+	};
+
+	// return core commands
+	public static Command[] getCommands()
+	{
+		return commands;
+	}
+}
