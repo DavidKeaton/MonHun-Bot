6d550cec2a8cba885a885aac194d7974e3658320
DavidKeaton <DavekDOS@gmail.com>
2017-02-23 15:22:30 -0500

    minor fleshing out of Weapon class

--- src/MonHun/Weapon.java
+++ src/MonHun/Weapon.java
@@ -27,23 +27,24 @@ public class Weapon
 	// power of weapon
 	private int power;
 	// element of weapon
+	private Element element;
+	// sharpness of weapon
+	private int sharpness;
 
 	/**
-	 * Create default weapon if none assigned, default = GREAT_SWORD
-	 */
-	public Weapon()
-	{
-		setWeaponType(WEAPON_TYPE.GREAT_SWORD);
-	}
-
-	/**
-	 * Create weapon based on `type'.
+	 * Only constructor for Weapon, requires all arguments.
 	 *
 	 * @param type      WEAPON_TYPE of weapon
+	 * @param element   @ref Element of weapon
+	 * @param power     raw power of weapon
+	 * @param sharpness sharpness of weapon, in numerical form
 	 */
-	public Weapon(WEAPON_TYPE type)
+	public Weapon(WEAPON_TYPE type, Element element, int power, int sharpness)
 	{
 		setWeaponType(type);
+		setElement(element);
+		setPower(power);
+		setSharpness(sharpness);
 	}
 
 	public WEAPON_TYPE getWeaponType()
@@ -54,5 +55,46 @@ public class Weapon
 	public void setWeaponType(WEAPON_TYPE type)
 	{
 		this.type = type;
+	}
+
+	public int getPower()
+	{
+		return power;
+	}
+
+	public void setPower(int power)
+	{
+		this.power = power;
+	}
+
+	public Element getElement()
+	{
+		return element;
+	}
+
+	public void setElement(Element element)
+	{
+		this.element = element;
+	}
+
+	public int getSharpness()
+	{
+		return sharpness;
+	}
+
+	public void setSharpness(int sharpness)
+	{
+		this.sharpness = sharpness;
+	}
+
+	/**
+	 * Convert an image representation of the sharpness bar into numbers
+	 *
+	 * @return      the numerical representation of the sharpness meter
+	 */
+	// TODO: convert sharpness meter image (pixel scan) into number values
+	private int convertSharpnessMeter(Object meter)
+	{
+		return 0;
 	}
 }
